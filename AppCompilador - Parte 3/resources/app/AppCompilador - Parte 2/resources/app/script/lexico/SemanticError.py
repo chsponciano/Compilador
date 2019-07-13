043d4c621ce1566fcf27a0a171ce080d1b7e19d3
Carlos <chs.ponciano@gmail.com>
2019-07-13 18:46:23 -0300

    Final

--- AppCompilador - Parte 3/resources/app/AppCompilador - Parte 2/resources/app/script/lexico/SemanticError.py
+++
@@ -1,5 +0,0 @@
-from AnalysisError import AnalysisError
-
-class SemanticError(AnalysisError):
-	def __init__(self, msg, position):
-		super(SemanticError, self).__init__(msg, position)		
--- AppCompilador - Parte 3/resources/app/script/SemanticError.py
+++
@@ -1,9 +0,0 @@
-from AnalysisError import AnalysisError
-
-class SemanticError(AnalysisError):
-	def __init__(self, msg, line):
-		super(SemanticError, self).__init__(msg, line)
-
-	def __str__(self):
-		return f'Erro na linha {self.line} - {self.msg}'
-	
